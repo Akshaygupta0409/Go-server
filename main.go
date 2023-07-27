fb4465f51760c7e4c138d5c3b705cdde66358b49
akshay kumar gupta <guptaakshay0409@gmail.com>
2023-07-27 18:37:26 +0530

    cors

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
+	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load("env")
-	portstring := os.Getenv("PORT")
+	//portstring := os.Getenv("PORT")
 	router := chi.NewRouter()
-	if portstring == "" {
-		log.Fatal("port is not available in this system")
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://x"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"x"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	srv := &http.Server{
+		Addr:    ":3000",
+		Handler: router,
 	}
-	fmt.Printf(portstring)
+	log.Printf("listing on PORT : %v", 3000)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
